e361e0735c816f4df8601f00db09ab7ea1723537
muh-adrian76 <muh.adriano76@gmail.com>
2025-04-15 17:20:57 +0700

    Fix Deploy Error

--- front-end/astro.config.mjs
+++ front-end/astro.config.mjs
@@ -2,17 +2,17 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 
 const DEV_PORT = 2121;
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.CI
-		? 'https://themesberg.github.io'
-		: `http://localhost:${DEV_PORT}`,
+	site: process.env.CI ? 'https://0.0.0.0' : `http://localhost:${DEV_PORT}`,
 	base: process.env.CI ? '/front-end' : undefined,
 
 	output: 'server',
+	adapter: vercel(),
 
 	/* Like Vercel, Netlify,… Mimicking for dev. server */
 	// trailingSlash: 'always',
